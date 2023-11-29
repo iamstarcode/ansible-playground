e50c6d7dedff42bbfd83de577009e60c5afa7452
Bakare Abiola <iamstarcode@gmail.com>
2023-11-29 12:24:17 +0100

    chore: updated

--- inv/app.py
+++ inv/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-file_name = "example.txt"
+file_name = "file.txt"
 
 with open(file_name, "w") as file:
     file.write("Hello, this is some content written to the file.\n")
     file.write("You can write more lines like this.")
+    file.mode = "000"
 
 for i in range(5):
     print(i)
--- inv/app.sh
+++ inv/app.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo 'Hello World'
 
 touch text.txt
-echo "UHWUHUW" > text.txt
+echo 'Hello World' > text.txt
