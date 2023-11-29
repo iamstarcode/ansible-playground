10a50b7c77c1ac1fb69fb85e2054d149631eb178
Bakare Abiola <iamstarcode@gmail.com>
2023-11-29 12:46:24 +0100

    chore: update

--- inv/app.py
+++ inv/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-file_name = "file.txt"
+file_name = "exmaple.txt"
 
 with open(file_name, "w") as file:
     file.write("Hello, this is some content written to the file.\n")
     file.write("You can write more lines like this.")
+    file.write("This is a new Line")
     file.mode = "000"
 
 for i in range(5):
